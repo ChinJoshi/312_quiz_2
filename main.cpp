e6e2341f0f2aa4f85230201a5f6c1384950b984f
ChinJoshi <cshjoshi@gmail.com>
2020-04-06 22:23:05 -0500

    working

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include "subsetSum.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 int main(){
+	int x[]={1,8,6,4,3};
+	printf("%d\n",subsetSum(x,5,2,9));
 }
--- subsetSum.cpp
+++ subsetSum.cpp
@@ -3,5 +3,19 @@
 #include "subsetSum.h"
 
 int subsetSum(int x[],int n,int k,int v){
-	return 1;
+	if(v==0 && k==0){
+		return true;
+	}
+	if(v!=0 && k==0){
+		return false;
+	}
+	if(n==0){
+		return false;
+	}
+	int result1=0;
+	if(v>=x[0]){
+		result1=subsetSum(x+1,n-1,k-1,v-x[0]);
+	}
+	int result2=subsetSum(x+1,n-1,k,v);
+	return (result1+result2)>0;
 }
